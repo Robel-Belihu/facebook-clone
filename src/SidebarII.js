672e08b53b4622f2002ec695865add88150f994a
RobelHoran <robelmelaku6@gmail.com>
2022-09-29 16:37:31 +0300

    edit the iframe

--- src/SidebarII.js
+++ src/SidebarII.js
@@ -4,8 +4,8 @@ import './SidebarII.css';
 function SidebarII() {
     return (
         <div className='sidebarII'>
-            <iframe className='frame' title='frame' src="https://www.youtube.com/"></iframe>
-
+            <iframe className='frame' src="https://www.youtube.com/embed/hHIikHJV9fI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className='frame' src="https://www.youtube.com/embed/W5qQJhe7sLE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
